45f039aad296a2d5a96b9b13b366ee251a2432e4
RuRick <grechishnikov94@bk.ru>
2019-08-10 17:04:36 +0700

    Add Webpack and Babel boilierplate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const Component = () => {
+    const element = document.createElement('div')
+
+    element.innerHTML = 'Hello, World!'
+
+    return element
+}
+
+document.body.appendChild(Component())
+
+module.hot && module.hot.accept()
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: 'google-chrome',
+        host: 'localhost',
+        port: 3000,
+        overlay: {
+            warnings: true,
+            errors: true,
+        },
+        progress: true,
+        hot: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+}
