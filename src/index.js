54e82e306a2ecd2a18048177c3275a2bf04bbee3
RuRick <grechishnikov94@bk.ru>
2019-08-11 20:40:29 +0700

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Route } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-import Header from './Components/Header'
-import Main from './Components/Main'
-import AddForm from './Components/AddForm'
-import Settings from './Components/Settings'
+import Header from './components/Header'
+import Main from './components/Main'
+import AddForm from './components/AddForm'
+import Settings from './components/Settings'
+import rootReducer from './reducers'
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
-    <Router>
-        <Header />
-        <Route path='/' exact component={Main} />
-        <Route path='/add' component={AddForm} />
-        <Route path='/settings' component={Settings} />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <Header />
+            <Route path='/' exact component={Main} />
+            <Route path='/add' component={AddForm} />
+            <Route path='/settings' component={Settings} />
+        </Router>
+    </Provider>,
     document.querySelector('#root')
 )
 
